47b202bf52ab9db11e5b57d8e70b2071b0e29f4a
John David Dela Cruz <johndaviddc1@gmail.com>
2023-12-01 10:48:06 +0800

    email api

---
+++ src/test/java/dave/dev/emailapi/EmailapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dave.dev.emailapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
