e09ac93d0c20d2eac19ba6e790f2af0e18b199f0
John David Dela Cruz <johndaviddc1@gmail.com>
2023-12-02 15:25:12 +0800

    added model package

---
+++ src/main/java/dave/dev/emailapi/model/Email.java
@@ -0,0 +1,59 @@
+package dave.dev.emailapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Email {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String sender;
+    private String recepient;
+    private String subject;
+    private String body;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getRecepient() {
+        return recepient;
+    }
+
+    public void setRecepient(String recepient) {
+        this.recepient = recepient;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
