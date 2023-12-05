67f276b1573ee1a54bcee2d099698f3c42b17de3
John David Dela Cruz <johndaviddc1@gmail.com>
2023-12-05 10:08:02 +0800

    added email service implementation

---
+++ src/main/java/dave/dev/emailapi/service/EmailServiceImpl.java
@@ -0,0 +1,24 @@
+package dave.dev.emailapi.service;
+
+import dave.dev.emailapi.model.Email;
+import dave.dev.emailapi.repository.EmailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    @Autowired
+    private EmailRepository emailRepository;
+
+    @Override
+    public Email sendEmail(Email email) {
+        return emailRepository.save(email);
+    }
+
+    @Override
+    public Email getEmailById(Long id) {
+        return emailRepository.findById(id)
+                .orElseThrow(() -> new EmailNotFoundException("Email not found with id: " + id));
+    }
+}
