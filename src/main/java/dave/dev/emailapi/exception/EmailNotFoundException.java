ee17dd272d2300320b802279fe846ae5a028bb82
John David Dela Cruz <johndaviddc1@gmail.com>
2023-12-06 13:25:01 +0800

    added exception package

---
+++ src/main/java/dave/dev/emailapi/exception/EmailNotFoundException.java
@@ -0,0 +1,7 @@
+package dave.dev.emailapi.exception;
+
+public class EmailNotFoundException extends RuntimeException {
+    public EmailNotFoundException(String message) {
+        super(message);
+    }
+}
