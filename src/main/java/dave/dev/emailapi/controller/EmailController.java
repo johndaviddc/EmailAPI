e38b0417eac3c65b1fa71dfa18f2f10742da119a
John David Dela Cruz <johndaviddc1@gmail.com>
2023-12-07 09:10:59 +0800

    added get mapping

--- src/main/java/dave/dev/emailapi/controller/EmailController.java
+++ src/main/java/dave/dev/emailapi/controller/EmailController.java
@@ -5,10 +5,7 @@ import dave.dev.emailapi.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/emails")
@@ -21,5 +18,11 @@ public class EmailController {
     public ResponseEntity<Email> sendEmail(@RequestBody Email email,) {
         Email sentEmail = emailService.sendEmail(email);
         return new ResponseEntity<>(sentEmail, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Email> getEmailById(@PathVariable Long id) {
+        Email email = emailService.getEmailById(id);
+        return new ResponseEntity<>(email, HttpStatus.OK);
     }
 }
